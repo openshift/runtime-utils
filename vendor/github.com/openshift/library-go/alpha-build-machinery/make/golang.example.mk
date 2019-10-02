ec81f9ca17873f6965951110aa48de8efe0cda05
Adam Kaplan <adam.kaplan@redhat.com>
2019-10-02 17:40:16 -0400

    Vendor alpha-build-machinery
    * Needed to get rehearsals to work

--- vendor/github.com/openshift/library-go/alpha-build-machinery/make/golang.example.mk
+++ vendor/github.com/openshift/library-go/alpha-build-machinery/make/golang.example.mk
@@ -8,6 +8,7 @@ GO_BUILD_PACKAGES :=./pkg/...
 # You can list all the golang related variables by:
 #   $ make -n --print-data-base | grep ^GO
 
-include vendor/github.com/openshift/library-go/alpha-build-machinery/make/golang.mk
+# Include the library makefile
+include ./golang.mk
 # All the available targets are listed in <this-file>.help
 # or you can list it live by using `make help`
