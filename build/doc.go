291000af71e3f965e97028711aa49e227f7776a8
gabemontero <gmontero@redhat.com>
2020-04-15 11:17:00 -0400

    bump(*): k8s 1.18.0, alpha-build-machinery

--- build/doc.go
+++ build/doc.go
@@ -2,5 +2,5 @@ package build
 
 import (
 	// Import alpha-build-machinery to get standard makefile targets
-	_ "github.com/openshift/library-go/alpha-build-machinery"
+	_ "github.com/openshift/build-machinery-go"
 )
